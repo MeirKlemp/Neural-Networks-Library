9e17f59fe5cea7bdd1e4d1b60c4269daffc328a9
MeirKlemp <251.klemp.work@gmail.com>
2020-01-06 13:11:41 +0200

    Documented the Matrix class.

--- NeuralNetworks/Matrix.cs
+++ NeuralNetworks/Matrix.cs
@@ -91,19 +91,42 @@ namespace NeuralNetworks
         #endregion
         #region Static Methods
         private static Random random = new Random();
-        public static Matrix GetRandomMatrix(int row, int col, int a = -1, int b = 1)
+        /// <summary>
+        /// Sets the random's seed.
+        /// </summary>
+        /// <param name="seed">The seed to set into the random.</param>
+        public static void SetRandomSeed(int seed)
+        {
+            random = new Random(seed);
+        }
+
+        /// <summary>
+        /// Creates a matrix with a random values.
+        /// </summary>
+        /// <param name="row">The new matrix's amount of rows.</param>
+        /// <param name="col">The new matrix's amount of columns.</param>
+        /// <param name="minValue">The inclusive minimum value of the random values.</param>
+        /// <param name="maxValue">The exlusive maximum value of the random values.</param>
+        /// <returns>The new matrix with the random values.</returns>
+        public static Matrix GetRandomMatrix(int row, int col, int minValue = -1, int maxValue = 1)
         {
             var matrix = new Matrix(row, col);
-            // Random value is in range [a, b]
-            matrix.Map((x) => random.NextDouble() * (b - a) + a);
+            // Random value is in range [a, b)
+            matrix.Map((x) => random.NextDouble() * (maxValue - minValue) + minValue);
             return matrix;
         }
 
+        /// <summary>
+        /// Calculates the matrix that is being created through the matrix multiplication of two given matrices.
+        /// </summary>
+        /// <param name="a">The first matrix.</param>
+        /// <param name="b">The second matrix.</param>
+        /// <returns>The calculated matrix from the matrix multiiplication from the two given matrices.</returns>
         public static Matrix MatMult(Matrix a, Matrix b)
         {
             if (a.Columns != b.Rows)
             {
-                throw new ArithmeticException("");
+                throw new ArithmeticException("For matrix multiplication matrix a's amount of columns must be equals to matrix b's amount of rows.");
             }
 
             var matrix = new Matrix(a.Rows, b.Columns);
@@ -121,6 +144,11 @@ namespace NeuralNetworks
             return matrix;
         }
 
+        /// <summary>
+        /// Transposes a given matrix.
+        /// </summary>
+        /// <param name="matrix">The matrix to transpose.</param>
+        /// <returns>The transposed matrix.</returns>
         public static Matrix Transpose(Matrix matrix)
         {
             var mat = new Matrix(matrix.Columns, matrix.Rows);
@@ -136,6 +164,12 @@ namespace NeuralNetworks
             return mat;
         }
 
+        /// <summary>
+        /// Creates a clone of the given matrix, then runs the map method on it.
+        /// </summary>
+        /// <param name="matrix">The matrix to clone.</param>
+        /// <param name="func">The function for the map.</param>
+        /// <returns>The mapped matrix.</returns>
         public static Matrix Map(Matrix matrix, Func<double, double> func)
         {
             matrix = new Matrix(matrix);
@@ -146,13 +180,33 @@ namespace NeuralNetworks
         public static bool EqualSize(Matrix a, Matrix b) => a.Rows == b.Rows && a.Columns == b.Columns;
         #endregion
         #region Fields
+        /// <summary>
+        /// An implementation of [] operators for the class in order to access into the matrix's values easly.
+        /// </summary>
+        /// <param name="row">The rows's index.</param>
+        /// <param name="col">The column's index.</param>
+        /// <returns></returns>
         public double this[int row, int col] { get => values[row, col]; set => values[row, col] = value; }
+        /// <summary>
+        /// The amount of the matrix's rows.
+        /// </summary>
         public int Rows { get; private set; }
+        /// <summary>
+        /// The amount of the matrix's columns.
+        /// </summary>
         public int Columns { get; private set; }
 
+        /// <summary>
+        /// The matrix's values.
+        /// </summary>
         private double[,] values;
         #endregion
         #region Ctors
+        /// <summary>
+        /// A constructor for creating an empty matrix with a specific size.
+        /// </summary>
+        /// <param name="rows">The new matrix's rows.</param>
+        /// <param name="columns">The new matrix's columns.</param>
         public Matrix(int rows, int columns)
         {
             Rows = rows;
@@ -161,6 +215,10 @@ namespace NeuralNetworks
             values = new double[Rows, Columns];
         }
 
+        /// <summary>
+        /// A constructor for creating a matrix filled with a 2d array's size and values.
+        /// </summary>
+        /// <param name="values">The 2d array.</param>
         public Matrix(double[,] values) : this(values.GetLength(0), values.GetLength(1))
         {
             for (int row = 0; row < Rows; row++)
@@ -172,11 +230,17 @@ namespace NeuralNetworks
             }
         }
 
+        /// <summary>
+        /// A constructor to clone a given matrix.
+        /// </summary>
+        /// <param name="matrix">The matrix to clone.</param>
         public Matrix(Matrix matrix) : this(matrix.values) { }
         #endregion
         #region Methods
-        public double[,] ToArray() => new Matrix(this).values;
-
+        /// <summary>
+        /// Runs every element of the matrix through a function.
+        /// </summary>
+        /// <param name="func">A function that gests a double and returns a double.</param>
         public void Map(Func<double, double> func)
         {
             for (int row = 0; row < Rows; row++)
@@ -188,8 +252,22 @@ namespace NeuralNetworks
             }
         }
 
+        /// <summary>
+        /// The implementation of the ICloneable's Clone function.
+        /// </summary>
+        /// <returns>A cloned matrix object.</returns>
         public object Clone() => new Matrix(this);
 
+        /// <summary>
+        /// Converts the matrix into a 2d array.
+        /// </summary>
+        /// <returns>The 2d array of the matrix.</returns>
+        public double[,] ToArray() => new Matrix(this).values;
+
+        /// <summary>
+        /// Convets the matrix into a representing string.
+        /// </summary>
+        /// <returns>The representing string.</returns>
         public override string ToString()
         {
             var str = "";
